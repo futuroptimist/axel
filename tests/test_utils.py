b5ccb1909672422fd7a66a6d7911ed04230848ce
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2025-08-10 22:33:43 -0700

    🐛: handle bytes in strip_ansi

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,3 +10,8 @@ def test_strip_ansi_handles_cursor_codes() -> None:
     """Non-color ANSI sequences should also be stripped."""
     text = "\x1b[2Kerror"
     assert strip_ansi(text) == "error"
+
+
+def test_strip_ansi_accepts_bytes() -> None:
+    """Byte strings should also be handled."""
+    assert strip_ansi(b"\x1b[31merror\x1b[0m") == "error"
